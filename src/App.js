0c6e44f9c47b4169268edfe532e56fc93a1f38ce
Braingenius2 <braingenius2@gmail.com>
2023-07-24 10:09:02 +0300

    Add route for /bookstore2

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
+          <Route path="/bookstore2" element={<HomePage />} />
           <Route path="/categories" element={<CategoriesPage />} />
         </Route>
       </Routes>
